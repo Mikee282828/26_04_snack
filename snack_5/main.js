f4b8744c95538da503eac1409705ba9ed3c07aa5
MikeDesktop <micheledong2803@gmail.com>
2024-04-27 01:30:16 +0200

    snack 5 - somma cifre

--- snack_5/main.js
+++ snack_5/main.js
@@ -1,5 +1,9 @@
 let number;
+let somma=0;
 while(!(number>999 && number <= 9999)){
     number = prompt("Inserisci un numero a 4 cifre");
 }
-console.log(number);
+for(i = 0; i<4; i++){
+    somma += parseInt(number[i]);
+}
+console.log("La somma delle cifre è:",somma);
